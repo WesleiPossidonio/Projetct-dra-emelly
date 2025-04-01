483d80fe1e459c0310ba0222ce742aa7301eb3b5
Weslei Possidonio <weslei.possidonio@gmail.com>
2025-04-01 15:29:42 -0300

    update Hero responsive

--- src/Components/Hero/index.tsx
+++ src/Components/Hero/index.tsx
@@ -1,17 +1,17 @@
 
 export const Hero = () => {
   return (
-    <section className="w-full  md:h-svh h-[52rem] p-5 md:p-16 lg:p-28 flex flex-col items-start justify-start 
-    lg:justify-center gap-2 bg-center bg-cover bg-no-repeat bg-bannerMobile md:bg-bannerDescktop md:bg-[-47rem_0] lg:bg-center">
-      <h1 className="md:w-[60%] text-3xl md:text-6xl text-white font-bold mt-24 md:mt-52 lg:mt-0">
+    <section className="w-full  md:h-svh h-[52rem] p-5 md:p-13 lg:p-14 xl:p-28 flex flex-col items-start justify-start 
+    lg:justify-center gap-2 bg-center bg-cover bg-no-repeat bg-bannerMobile md:bg-bannerDescktop md:bg-[-52rem_0] lg:bg-[-43rem_0]">
+      <h1 className="md:w-[60%] text-3xl md:text-5xl text-white font-bold mt-20 md:mt-52 lg:mt-0">
         Excelência  e  <br /> Comprometimento  com o Cliente
       </h1>
-      <p className="md:w-[60%] text-white text-lg md:text-xl mt-2">
+      <p className="md:w-[60%] text-white md:text-lg lg:text-xl mt-2">
         Compromisso em oferecer atendimento personalizado, compreendendo cada
         caso de forma única e buscando sempre a melhor resolução, com eficiência
         e sensibilidade na atuação jurídica seja no âmbito extrajudicial ou judicial.
       </p>
-      <button className="w-36 self-start text-lg bg-white px-6 py-3 mt-4 rounded text-greenTheme font-bold">Saiba Mais!</button>
+      <button className="w-36 self-start text-lg bg-white px-6 py-3 mt-2 md:mt-4 rounded text-greenTheme font-bold">Saiba Mais!</button>
     </section>
   )
 }
