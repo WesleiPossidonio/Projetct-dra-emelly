748d11d9fe005b586f69a39968db591d88a4df9e
Weslei Possidonio <weslei.possidonio@gmail.com>
2025-04-01 15:11:54 -0300

    update hero

--- src/Components/Hero/index.tsx
+++ src/Components/Hero/index.tsx
@@ -1,9 +1,9 @@
 
 export const Hero = () => {
   return (
-    <section className="w-full md:h-svh h-[52rem] p-5 md:p-16 flex flex-col items-start justify-start 
+    <section className="w-full  md:h-svh h-[52rem] p-5 md:p-16 lg:p-28 flex flex-col items-start justify-start 
     lg:justify-center gap-2 bg-center bg-cover bg-no-repeat bg-bannerMobile md:bg-bannerDescktop md:bg-[-47rem_0] lg:bg-center">
-      <h1 className=" md:w-[60%] text-3xl md:text-6xl text-white font-bold mt-24 md:mt-52 lg:mt-0">
+      <h1 className="md:w-[60%] text-3xl md:text-6xl text-white font-bold mt-24 md:mt-52 lg:mt-0">
         Excelência  e  <br /> Comprometimento  com o Cliente
       </h1>
       <p className="md:w-[60%] text-white text-lg md:text-xl mt-2">
